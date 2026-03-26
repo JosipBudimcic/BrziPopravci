e0d826058917bb7d0fda4474ba89d2ab0da9e644
JosipBudimcic <budoliki@gmail.com>
2026-03-26 18:23:11 +0100

    v0.2
    pocetna stranica radi

--- src/constants copy.js
+++
@@ -1,8 +0,0 @@
-export const IME_APLIKACIJE='Edunova APP'
-
-export const RouteNames = {
-    HOME: '/',
-    SMJEROVI: '/smjerovi', // ovo je ruta
-    SMJEROVI_NOVI: '/smjerovi/novi',
-    SMJEROVI_PROMJENA: '/smjerovi/:sifra'
-}
